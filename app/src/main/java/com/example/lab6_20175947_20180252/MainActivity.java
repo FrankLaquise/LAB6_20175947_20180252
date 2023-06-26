7f777c4296af7811527f079162c4c9a672021415
FrankLaquise <a20175947@pucp.edu.pe>
2023-06-26 11:12:10 -0500

    Initial commit

---
+++ app/src/main/java/com/example/lab6_20175947_20180252/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.lab6_20175947_20180252;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
